cc0b734326f9614d6bb77360307725b8d7be4351
johnnyeven_work <johnnyeven@gmail.com>
2014-09-25 11:40:03 +0800

    1

--- monsterinfo.php
+++ monsterinfo.php
@@ -6,8 +6,31 @@ $json = array(
 		'x'		=>	.5,
 		'y'		=>	.25
 	),
-	'speed'			=>	250,
-	'attackRange'	=>	50
+	'atk'			=>	140,
+	'matk'			=>	0,
+	'def'			=>	100,
+	'mdef'			=>	150,
+	'flee'			=>	100,
+	'crit'			=>	90,
+	'speed'			=>	200,
+	'health'		=>	500,
+	'attackRange'	=>	80,
+	'attackRate'	=>	2,
+	'battle'	=>	array(
+		'normal'	=>	array(
+			'type'		=>	0,		//0=只有一个动作 1=连招 2=连续播放
+			'action'	=>	array('attack1'),
+			'delay'		=>	array(0.5)		//伤害计算延时
+		),
+		'skills'	=>	array(
+			array(
+				'name'		=>	'重击',
+				'action'	=>	array('attack2'),
+				'effect'	=>	array(),
+				'delay'		=>	array(0.7)
+			)
+		)
+	)
 );
 
 echo json_encode($json);
